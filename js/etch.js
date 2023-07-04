4439db655cefc80ab73ebb88b052a9577e9eb929
Kevin Jackman <kjdevedjk@gmail.com>
2023-07-04 18:13:30 -0400

    Add 1 grid element

--- js/etch.js
+++ js/etch.js
@@ -1,1 +1,19 @@
 /* javascript for the Odin Project Etch-A-Sketch */
+console.log('I am here');
+const theGrid = document.createElement('div');
+
+theGrid.style.color = 'blue';
+theGrid.style.width = '16px';
+theGrid.style.height = '16px';
+theGrid.style.borderColor = 'brown';
+theGrid.style.borderStyle = 'double';
+theGrid.textContent = '1';
+theGrid.style.fontSize = '10px';
+
+
+console.log('I am here');
+const theField = document.querySelector('#theBoard');
+console.log(theField);
+theField.appendChild(theGrid);
+
+console.log('I am here');
